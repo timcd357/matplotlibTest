3e7ef97da47e335c7bd6e1f4137bef8cb66ef41d
timcd <854950310@qq.com>
2020-12-15 11:24:47 +0800

    稍微修改了一点，功能不变

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -6,13 +7,15 @@ import pandas as pd
 
 
 def test():
-    xls = pd.read_excel("C:\\Users\\timcd\\Desktop\\出库.XLS", date_parser=['2020-01-01'])
+    xls = pd.read_excel("C:\\Users\\timcd\\Desktop\\出库.XLS")
     # xls["出库时间"] = pd.to_datetime(xls.出库时间, format="%Y.%m")
     xls["出库时间"] = xls["出库时间"].str[0:7]
-    xls.to_excel("C:\\Users\\timcd\\Desktop\\1.xls")
+    # xls.to_excel("C:\\Users\\timcd\\Desktop\\1.xls")
+
     sum_name = xls.groupby("品名/品规").sum()[["数量", "无税金额"]]
     sum_name = sum_name.sort_values("无税金额", ascending=False)
     sum_name.to_excel("C:\\Users\\timcd\\Desktop\\按品名.xls")
+
     sum_cus = xls.groupby("客户名称").sum()[["数量", "无税金额"]]
     sun_cus = sum_cus.sort_values("无税金额", ascending=False)
     sum_cus.to_excel("C:\\Users\\timcd\\Desktop\\按客户.xls")
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\work\python\Matplotlib\venv
+set VIRTUAL_ENV=E:\work\python\matplotlibTest\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
